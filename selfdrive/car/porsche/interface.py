b2f03eeefb4d6dcdba0f44e64e5eb68e2441f926
kon <k@smo.la>
2021-11-28 18:35:38 -0800

    more changes from master

--- selfdrive/car/porsche/interface.py
+++ selfdrive/car/porsche/interface.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from cereal import car
 from selfdrive.config import Conversions as CV
+from selfdrive.car.porsche.tunes import LatTunes, LongTunes, set_long_tune, set_lat_tune
 from selfdrive.car.porsche.values import Ecu, CAR, TSS2_CAR, MIN_ACC_SPEED, PEDAL_HYST_GAP, CarControllerParams
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.interfaces import CarInterfaceBase
@@ -17,7 +18,7 @@ class CarInterface(CarInterfaceBase):
   def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=[]):  # pylint: disable=dangerous-default-value
     ret = CarInterfaceBase.get_std_params(candidate, fingerprint)
 
-    ret.carName = "toyota"
+    ret.carName = "porsche"
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.toyota)]
 
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
@@ -30,23 +31,14 @@ class CarInterface(CarInterfaceBase):
     #   ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
 
     if candidate == CAR.NINE4x4:
-      # steerRatioRear @21 :Float32;  # [] ratio of steering wheel angle to rear wheel angle (usually 0)
-      stop_and_go = True
       ret.safetyConfigs[0].safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
-      ret.wheelbase = 2.70      # [m] distance from rear axle to front axle
-      ret.steerRatio = 15.74   # [] ratio of steering wheel angle to front wheel angle
+      stop_and_go = True
+      ret.wheelbase = 2.70     # [m] distance from rear axle to front axle
+      ret.steerRatio = 15.74   # unknown end-to-end spec
       tire_stiffness_factor = 0.6371   # hand-tune
-      ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG       # [kg] curb weight: all fluids no cargo
+      ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
 
-      ret.lateralTuning.init('indi')
-      ret.lateralTuning.indi.innerLoopGainBP = [0.]
-      ret.lateralTuning.indi.innerLoopGainV = [4.0]
-      ret.lateralTuning.indi.outerLoopGainBP = [0.]
-      ret.lateralTuning.indi.outerLoopGainV = [3.0]
-      ret.lateralTuning.indi.timeConstantBP = [0.]
-      ret.lateralTuning.indi.timeConstantV = [1.0]
-      ret.lateralTuning.indi.actuatorEffectivenessBP = [0.]
-      ret.lateralTuning.indi.actuatorEffectivenessV = [1.0]
+      set_lat_tune(ret.lateralTuning, LatTunes.NINE_4x4)
       ret.steerActuatorDelay = 0.3
 
     ret.steerRateCost = 1.
@@ -66,7 +58,7 @@ class CarInterface(CarInterfaceBase):
     smartDsu = 0x2FF in fingerprint[0]
     # In TSS2 cars the camera does long control
     found_ecus = [fw.ecu for fw in car_fw]
-    ret.enableDsu = (len(found_ecus) > 0) and (Ecu.dsu not in found_ecus) and (candidate not in NO_DSU_CAR)
+    ret.enableDsu = (len(found_ecus) > 0) and (Ecu.dsu not in found_ecus) and (candidate not in smartDsu)
     ret.enableGasInterceptor = 0x201 in fingerprint[0]
     # if the smartDSU is detected, openpilot can send ACC_CMD (and the smartDSU will block it from the DSU) or not (the DSU is "connected")
     ret.openpilotLongitudinalControl = smartDsu or ret.enableDsu or candidate in TSS2_CAR
@@ -80,28 +72,13 @@ class CarInterface(CarInterfaceBase):
     ret.communityFeature = ret.enableGasInterceptor or ret.enableDsu or smartDsu
 
     if ret.enableGasInterceptor:
-      ret.longitudinalTuning.kpBP = [0., 5., MIN_ACC_SPEED, MIN_ACC_SPEED + PEDAL_HYST_GAP, 35.]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.765, 2.255, 1.5]
-      ret.longitudinalTuning.kiBP = [0., MIN_ACC_SPEED, MIN_ACC_SPEED + PEDAL_HYST_GAP, 35.]
-      ret.longitudinalTuning.kiV = [0.18, 0.165, 0.489, 0.36]
+      set_long_tune(ret.longitudinalTuning, LongTunes.PEDAL)
     # elif candidate in [CAR.COROLLA_TSS2, CAR.COROLLAH_TSS2, CAR.RAV4_TSS2, CAR.RAV4H_TSS2, CAR.LEXUS_NX_TSS2]:
-    #   # Improved longitudinal tune
-    #   ret.longitudinalTuning.deadzoneBP = [0., 8.05]
-    #   ret.longitudinalTuning.deadzoneV = [.0, .14]
-    #   ret.longitudinalTuning.kpBP = [0., 5., 20.]
-    #   ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
-    #   ret.longitudinalTuning.kiBP = [0., 5., 12., 20., 27.]
-    #   ret.longitudinalTuning.kiV = [.35, .23, .20, .17, .1]
+    #   set_long_tune(ret.longitudinalTuning, LongTunes.TSS2)
     #   ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
     #   ret.startingAccelRate = 6.0  # release brakes fast
     else:
-      # Default longitudinal tune
-      ret.longitudinalTuning.deadzoneBP = [0., 9.]
-      ret.longitudinalTuning.deadzoneV = [0., .15]
-      ret.longitudinalTuning.kpBP = [0., 5., 35.]
-      ret.longitudinalTuning.kiBP = [0., 35.]
-      ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
-      ret.longitudinalTuning.kiV = [0.54, 0.36]
+      set_long_tune(ret.longitudinalTuning, LongTunes.TSS)
 
     return ret
 
@@ -140,7 +117,7 @@ class CarInterface(CarInterfaceBase):
   # to be called @ 100hz
   def apply(self, c):
 
-    can_sends = self.CC.update(c.enabled, self.CS, self.frame,
+    can_sends = self.CC.update(c.enabled, c.active, self.CS, self.frame,
                                c.actuators, c.cruiseControl.cancel,
                                c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
                                c.hudControl.rightLaneVisible, c.hudControl.leadVisible,
